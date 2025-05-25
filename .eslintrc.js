dd67d2b30091bf187891bc248a88c404d7b51f00
Ronnie Lutalo <ronnielutaro@gmail.com>
2025-05-25 14:19:08 +0300

    fix: use CommonJS syntax in ESLint config for compatibility
    - Removed any ES module imports from .eslintrc.js
    - Ensured config uses only module.exports and CommonJS syntax
    - This resolves ESLint config loading errors and restores linting functionality

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'react-hooks',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'react/react-in-jsx-scope': 'off',
+  },
+  ignorePatterns: ['node_modules/', '.next/', 'dist/', 'public/'],
+};
